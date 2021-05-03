6cb3af1ef08923ad1aed3d2a9c71a6647a7da9af
NormanSv <game260@mail.ru>
2021-05-03 23:44:35 +0300

    feat: add search script

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -1,7 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.tooltipped');
-    var instances = M.Tooltip.init(elems, options);
+    var instances = M.Tooltip.init(elems);
 });
 $(document).ready(function() {
     $('input#input_text, textarea#textarea2').characterCounter();
 });
+document.getElementById('search').addEventListener('input',()=>{
+    fetch(`http://localhost:8080/suggest?query=${document.getElementById('search').value}`,{
+        method:'post',
+        headers: new Headers({
+            'Autorization':'Basic'
+        })
+    })
+        .then((response) => response.json())
+        .then((responseJson) => {
+            console.log(responseJson, 'res JSON');
+            if (responseJson.status == "success") {
+                console.log(this.state);
+                alert("okeYYYY!!");
+            }
+        });
+})
