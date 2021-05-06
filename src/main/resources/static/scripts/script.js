7824094ea61ec54a59a6ddae44e8233ec77216c9
NormanSv <game260@mail.ru>
2021-05-06 11:43:08 +0300

    feat: add search

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -5,19 +5,67 @@ document.addEventListener('DOMContentLoaded', function() {
 $(document).ready(function() {
     $('input#input_text, textarea#textarea2').characterCounter();
 });
-document.getElementById('search').addEventListener('input',()=>{
-    fetch(`http://localhost:8080/suggest?query=${document.getElementById('search').value}`,{
-        method:'post',
-        headers: new Headers({
-            'Autorization':'Basic'
+// document.getElementById('search').addEventListener('keydown', function(e) {
+//     if (e.keyCode === 13) {
+//         fetch(`http://localhost:8080/suggest?query=${document.getElementById('search').value}`,{
+//             method:'post',
+//             headers: new Headers({
+//                 'Autorization':'Basic'
+//             })
+//         })
+//             .then((response) => response.json())
+//             .then((responseJson) => {
+//                 console.log(responseJson);
+//                 document.getElementById('searchResults').innerHTML="";
+//                 responseJson.forEach(item =>{
+//                     const {
+//                         username:username,
+//                         picturePath:picturePath,
+//                         id
+//                     } = item;
+//                     const card = document.createElement('li');
+//                     card.innerHTML = `
+//                             <a href="/profile/${username}" class="follower">
+//                                 <img src="${picturePath}" alt="fff" class="user-avatar">
+//                                 <span class="username">${username}</span>
+//                             </a>
+//                         `;
+//                     document.getElementById('searchResults').append(card);
+//                 });
+//             });
+//     }
+// });
+document.getElementById('search').addEventListener('input', function(e) {
+
+        fetch(`http://localhost:8080/suggest?query=${document.getElementById('search').value}`,{
+            method:'post',
+            headers: new Headers({
+                'Autorization':'Basic'
+            })
         })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                document.getElementById('searchResults').innerHTML="";
+                responseJson.forEach(item =>{
+                    const {
+                        username:username,
+                        picturePath:picturePath,
+                        id
+                    } = item;
+                    const card = document.createElement('li');
+                    card.innerHTML = `
+                            <a href="/profile/${username}" class="follower">
+                                <img src="${picturePath}" alt="fff" class="user-avatar">
+                                <span class="username">${username}</span>
+                            </a>
+                        `;
+                    document.getElementById('searchResults').append(card);
+                });
+            });
+
+});
+document.querySelectorAll('.backBtn').forEach(item=>{
+    item.addEventListener('click',()=>{
+        history.go(-1);
     })
-        .then((response) => response.json())
-        .then((responseJson) => {
-            console.log(responseJson, 'res JSON');
-            if (responseJson.status == "success") {
-                console.log(this.state);
-                alert("okeYYYY!!");
-            }
-        });
 })
